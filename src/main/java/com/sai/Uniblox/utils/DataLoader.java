30838e389b3f9577f274e6bf3c77e12f7bc38cfa
saiashish9 <saiashish3760@gmail.com>
2024-08-14 04:29:35 +0530

    [Uniblox Backend] - Completed

--- src/main/java/com/sai/Uniblox/utils/DataLoader.java
+++ src/main/java/com/sai/Uniblox/utils/DataLoader.java
@@ -8,18 +8,19 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-@UtilityClass
+@UtilityClass // Marks this class as a utility class with only static methods
 public class DataLoader {
 
+    // Generic method to read data from a JSON file and map it to a List of type T
     public static <T> List<T> readDataFromFile(String type, Class<T> clazz) {
         ObjectMapper objectMapper = new ObjectMapper();
         List<T> dataList = null;
 
         try {
-            // Construct CollectionType for List<T>
+            // Construct CollectionType for List<T>, to specify the type of the collection
             CollectionType listType = objectMapper.getTypeFactory().constructCollectionType(List.class, clazz);
 
-            // Read data from the file
+            // Read data from the JSON file, converting it into the specified list type
             dataList = objectMapper.readValue(new File(String.format("%s.json", type)), listType);
         } catch (IOException e) {
             e.printStackTrace();
